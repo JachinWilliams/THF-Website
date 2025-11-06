191bc3a71c32769ce1ada4fa2447873444572c73
JachinWilliams <washjaywill@gmail.com>
2025-11-06 13:38:37 -0400

    Fixing programs DB

--- Databases.js
+++ Databases.js
@@ -196,154 +196,60 @@ var programmers = [
 ];
 
 var programs = [
-    {
-        name: "Shape Tool [0.9]",
-        author: "Ace Rogers",
-        link: "https://www.khanacademy.org/computer-programming/shape-tool-09-thf/5494695009894400",
-        year: "pj2024"
-    },
-    {
-        name: "The Feast of the Dragonfly",
-        author: "Ace Rogers and Duke",
-        link: "https://www.khanacademy.org/computer-programming/the-feast-of-the-dragonfly-thf/5814235201716224",
-        year: "pj2024"
-    },
-    {
-        name: "Peaceful Studying",
-        author: "marci",
-        link: "https://www.khanacademy.org/computer-programming/peaceful-studying-the-hotlist-forge/6645258246340608",
-        year: "pj2024"
-    },
-    {
-        name: "KhanBot v2",
-        author: "LeaferStudios",
-        link: "https://www.khanacademy.org/computer-programming/khanbot-v2/5837126102335488",
-        year: "pj2024"
-    },
-    {
-        name: "STARFLEET",
-        author: "Liam K.",
-        link: "https://www.khanacademy.org/computer-programming/starfleet-thf/5890582741696512",
-        year: "pj2024"
-    },
-    {
-        name: "it's a ghost",
-        author: "F4stB01",
-        link: "https://www.khanacademy.org/computer-programming/its-a-ghost/4778671549628416",
-        year: "pj2024"
-    },
-    {
-        name: "unnamed game",
-        author: "Levi F.",
-        link: "https://www.khanacademy.org/computer-programming/unnamed-game-for-the-hotlist-forge/5665590215098368",
-        year: "pj2024"
-    },
-    
-    {
-        name: "YOU FOUGHT IN THE CLONE WARS!?!?!?!?",
-        author: "The Marvelous Cracker",
-        link: "https://www.khanacademy.org/computer-programming/you-fought-in-the-clone-wars/6208583806795776",
-        year: "pj2023"
-    },
-    {
-        name: "Sierpinski Triangle with Recursion",
-        author: "The Marvelous Cracker",
-        link: "https://www.khanacademy.org/computer-programming/sierpinski-triangle-with-recursion/5150337792786432",
-        year: "pj2023"
-    },
-    {
-        name: "AVATAR WAR II",
-        author: "Captain Longtreader",
-        link: "https://www.khanacademy.org/computer-programming/avatar-war-ii/5594832883728384",
-        year: "pj2023"
-    },
-    {
-        name: "Danger Zone | Full Game",
-        author: "Captain Longtreader",
-        link: "https://www.khanacademy.org/computer-programming/danger-zone-full-game/4637965630095360",
-        year: "pj2023"
-    },
-    {
-        name: "Me",
-        author: "Birdwatcher14",
-        link: "https://www.khanacademy.org/computer-programming/me/4828416974831616",
-        year: "pj2023"
-    },
-    {
-        name: "Spider-Man: No Way Home",
-        author: "Ace Rogers",
-        link: "https://www.khanacademy.org/computer-programming/spider-man-no-way-home/4812187602042880",
-        year: "pj2023"
-    },
-    {
-        name: "The Programmer's Quiz [OLD]",
-        author: "CoraL",
-        link: "https://www.khanacademy.org/computer-programming/the-programmers-quiz-old/5324397868793856",
-        year: "pj2023"
-    },
-    {
-        name: "Can Chess, with Hexagons?",
-        author: "Cavan P",
-        link: "https://www.khanacademy.org/computer-programming/can-chess-with-hexagons/5603434491068416",
-        year: "pj2023"
-    },
-    {
-        name: "About Snakes FIRST DRAFT",
-        author: "Cedar",
-        link: "https://www.khanacademy.org/computer-programming/about-snakes-first-draft/6708314365542400",
-        year: "pj2023"
-    },
-    {
-        name: "Mission Delta",
-        author: "Duke",
-        link: "https://www.khanacademy.org/computer-programming/new-program/4745873322524672",
-        year: "pj2025"
-    },
-    {
-        name: "Lego Brawls",
-        author: "Avenger",
-        link: "https://www.khanacademy.org/computer-programming/i/5254136497553408",
-        year: "pj2025"
-    },
-    {
-        name: "Peaceful Music of Nature",
-        author: "Ace Rogers",
-        link: "https://www.khanacademy.org/computer-programming/peaceful-music-of-nature/5934697620815872",
-        year: "pj2025"
-    },
-    {
-        name: "Playful Puppy",
-        author: "Ace Rogers",
-        link: "https://www.khanacademy.org/computer-programming/playful-puppy/6754175648645120",
-        year: "pj2025"
-    },
-    {
-        name: "Reckless Racer 3D Beta",
-        author: "Galaxy Ranger",
-        link: "https://www.khanacademy.org/computer-programming/reckless-racer-3d-beta-thf/6389186362458112",
-        year: "pj2025"
-    },
-    {
-        name: "Joan of Arc",
-        author: "Arwen",
-        link: "https://www.khanacademy.org/computer-programming/joan-of-arc/4891186406473728",
-        year: "pj2025"
-    },
-    {
-        name: "Tower of Time",
-        author: "Quantum Cat",
-        link: "https://www.khanacademy.org/computer-programming/tower-of-time-thfgame/5222466763472896",
-        year: "pj2025"
-    },
-    {
-        name: "Blue 2",
-        author: "Ibraheem Ahmed",
-        link: "https://www.khanacademy.org/computer-programming/blue-2-platformer/5229007159803904",
-        year: "pj2025"
-    }
-
-
-
+  {
+    name: "Neon Grid Mapper",
+    author: "Ava Sterling",
+    link: "https://upload.wikimedia.org/wikipedia/commons/3/3b/Javascript_Logo.png",
+    year: "pj2025"
+  },
+  {
+    name: "Quantum Bounce Simulator",
+    author: "Leo Vortex",
+    link: "https://upload.wikimedia.org/wikipedia/commons/3/3b/Javascript_Logo.png",
+    year: "pj2025"
+  },
+  {
+    name: "Pixel Forge [v2.1]",
+    author: "Nova Quinn",
+    link: "https://upload.wikimedia.org/wikipedia/commons/3/3b/Javascript_Logo.png",
+    year: "pj2025"
+  },
+  {
+    name: "Draw Tool",
+    author: "John Doe",
+    link: "https://upload.wikimedia.org/wikipedia/commons/3/3b/Javascript_Logo.png",
+    year: "pj2024"
+  },
+  {
+    name: "Color Cascade Engine",
+    author: "Mira Solis",
+    link: "https://upload.wikimedia.org/wikipedia/commons/3/3b/Javascript_Logo.png",
+    year: "pj2024"
+  },
+  {
+    name: "Logic Maze Builder",
+    author: "Ezra Vale",
+    link: "https://upload.wikimedia.org/wikipedia/commons/3/3b/Javascript_Logo.png",
+    year: "pj2024"
+  },
+  {
+    name: "Retro Pixel Racer",
+    author: "Finn Wilder",
+    link: "https://upload.wikimedia.org/wikipedia/commons/3/3b/Javascript_Logo.png",
+    year: "pj2023"
+  },
+  {
+    name: "Code Rhythm Tracker",
+    author: "Zara Flint",
+    link: "https://upload.wikimedia.org/wikipedia/commons/3/3b/Javascript_Logo.png",
+    year: "pj2023"
+  },
+  {
+    name: "Sketch Synth Lab",
+    author: "Jett Nova",
+    link: "https://upload.wikimedia.org/wikipedia/commons/3/3b/Javascript_Logo.png",
+    year: "pj2023"
+  }
 ];
 
 export {programmers, programs};
